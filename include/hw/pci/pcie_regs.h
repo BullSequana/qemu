fb40028b755b1730328e3651cf47f975311b4835
Clément Mathieu--Drif <clement.mathieu--drif@eviden.com>
2024-03-26 09:08:43 +0100

    pcie: add helper to declare PASID capability for a pcie device
    Signed-off-by: Clément Mathieu--Drif <clement.mathieu--drif@eviden.com>

--- include/hw/pci/pcie_regs.h
+++ include/hw/pci/pcie_regs.h
@@ -86,6 +86,9 @@ typedef enum PCIExpLinkWidth {
 #define PCI_ARI_VER                     1
 #define PCI_ARI_SIZEOF                  8
 
+/* PASID */
+#define PCI_PASID_VER                   1
+#define PCI_EXT_CAP_PASID_MAX_WIDTH     20
 /* AER */
 #define PCI_ERR_VER                     2
 #define PCI_ERR_SIZEOF                  0x48
