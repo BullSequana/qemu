5a4b15f9e5d517cd6f20530a81c0f747c94bc69e
Clement Mathieu--Drif <clement.mathieu--drif@eviden.com>
2024-11-27 09:51:31 +0100

    intel_iommu: Implement the get_address_space_pasid iommu operation
    Signed-off-by: Clement Mathieu--Drif <clement.mathieu--drif@eviden.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -4845,15 +4845,21 @@ static void vtd_reset(DeviceState *dev)
     vtd_address_space_refresh_all(s);
 }
 
-static AddressSpace *vtd_host_dma_iommu(PCIBus *bus, void *opaque, int devfn)
+static AddressSpace *vtd_host_dma_iommu_pasid(PCIBus *bus, void *opaque,
+                                              int devfn, uint32_t pasid)
 {
     IntelIOMMUState *s = opaque;
     VTDAddressSpace *vtd_as;
 
     assert(0 <= devfn && devfn < PCI_DEVFN_MAX);
 
-    vtd_as = vtd_find_add_as(s, bus, devfn, PCI_NO_PASID);
+    vtd_as = vtd_find_add_as(s, bus, devfn, pasid);
     return &vtd_as->as;
+}
+
+static AddressSpace *vtd_host_dma_iommu(PCIBus *bus, void *opaque, int devfn)
+{
+    return vtd_host_dma_iommu_pasid(bus, opaque, devfn, PCI_NO_PASID);
 }
 
 static IOMMUMemoryRegion *vtd_get_memory_region_pasid(PCIBus *bus,
@@ -5122,6 +5128,7 @@ static int vtd_iommu_pri_request_page(IOMMUMemoryRegion *iommu, hwaddr addr,
 
 static PCIIOMMUOps vtd_iommu_ops = {
     .get_address_space = vtd_host_dma_iommu,
+    .get_address_space_pasid = vtd_host_dma_iommu_pasid,
     .get_memory_region_pasid = vtd_get_memory_region_pasid,
     .set_iommu_device = vtd_dev_set_iommu_device,
     .unset_iommu_device = vtd_dev_unset_iommu_device,
